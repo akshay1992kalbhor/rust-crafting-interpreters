e0d1fa432460feadbe512aa1d1434fd005142530
akshay1992kalbhor <akshay1992kalbhor@gmail.com>
2022-05-21 20:40:51 +0530

    Complete scanner implementation

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,4 @@
-fn test1(s: &str) {
-    println!("S: {}", s);
-}
-
-fn fun1() {
-    // borrowed types: &str, &[T], &T
-    // borrowing an owned type: &String, &Vec<T>, &Box<T>
-    // "123" = string slice = &str
-    let s1 = String::from("123");
-    let s3 = &s1 as &str;
-    let s2 = &s1[..];
-    test1(s2);
-    let l1: Box<String> = Box::from(s1);
-}
-
-use rust_crafting_interpreters::*;
+use rust_crafting_interpreters::scanner::Scanner;
 
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
